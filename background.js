ce2bd155ded7a44da5d07be51bf9f0f111a0c46c
Stefan Manciu <manciustefan@gmail.com>
2021-03-23 00:27:50 +0200

    cleaned up

--- background.js
+++
@@ -1,9 +0,0 @@
-var onCompleted = function(details) {
-  const Http = new XMLHttpRequest();
-  const url = details.url;
-  console.log(details);
-}
-
-chrome.webRequest.onCompleted.addListener(onCompleted, {
-  urls: ["https://programare.vaccinare-covid.gov.ro/*"]
-}, ["responseHeaders"])
